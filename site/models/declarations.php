c401ae9813f9b315bc3c608fa5c8df36ca1a39fd
xillibit <xillibit@gmail.com>
2014-12-17 16:19:50 +0100

    Typo after last commit

--- site/models/declarations.php
+++ site/models/declarations.php
@@ -1,6 +1,6 @@
 <?php
 /**
-bv * @package		Joomla.Site
+ * @package		Joomla.Site
  * @subpackage	com_contact
  * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
